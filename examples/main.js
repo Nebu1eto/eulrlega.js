311c30f4d359e00096fa2df6c969f48c1118c313
Kihan Park <SteelHeart@SteelHeart-MacBookPro.local>
2013-03-14 17:08:10 +0900

    First Commit

---
+++ examples/main.js
@@ -0,0 +1,7 @@
+var eulrlega = require('./eulrlega.js');
+var a = '고기';
+var b = ' 먹고 싶다.\n';
+
+console.log(a + eulrlega.getEulrl(a) + b);
+console.log(a + eulrlega.getEga(a) + b);
+console.log(a + eulrlega.getEunnn(a) + b);
