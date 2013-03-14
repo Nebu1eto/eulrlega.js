88719d5a89e91d0981c445c275cf4e142fcb3ab7
Kihan Park <SteelHeart@SteelHeart-MacBookPro.local>
2013-03-14 18:41:07 +0900

    0.1.0_Modified

--- examples/main.js
+++ examples/main.js
@@ -1,7 +1,2 @@
 var eulrlega = require('./eulrlega.js');
-var a = '고기';
-var b = ' 먹고 싶다.\n';
 
-console.log(a + eulrlega.getEulrl(a) + b);
-console.log(a + eulrlega.getEga(a) + b);
-console.log(a + eulrlega.getEunnn(a) + b);
