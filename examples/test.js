37f5b2fbb13da7baca964b69ed7450f09f98f604
Kihan Park <SteelHeart@SteelHeart-MacBookPro.local>
2013-03-18 01:29:31 +0900

    getSentence function added :D

---
+++ examples/test.js
@@ -0,0 +1,8 @@
+// Isn't npm not uploaded, it cannot work. :(
+// instead eulrlega.js's path on 'eulrlega' :D
+var eulrlega = require('eulrlega');
+
+
+console.log(eulrlega.getSentence("나#c 치킨#b 먹고싶다."));
+console.log(eulrlega.getSentence("배#b 너무 고프다."));
+console.log(eulrlega.getSentence("누가 나에게 치킨#a 사줬으면 좋겠다."));
